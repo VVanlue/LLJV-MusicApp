bf7ab3b7d825bfddaef1867ecc4443dd2250a942
VVanlue <victoriavanlue8@gmail.com>
2025-02-27 01:55:43 -0500

    created the song, omg that took a long time

--- lljvjavafx/src/main/java/com/program/MusicPlayer.java
+++ lljvjavafx/src/main/java/com/program/MusicPlayer.java
@@ -8,34 +8,89 @@ public class MusicPlayer {
     public void playSong() {
         try {
             playLine1();
-            Thread.sleep(300);
+            Thread.sleep(100);
             playLine2();
-            Thread.sleep(300);
+            Thread.sleep(100);
             playLine3();
-            Thread.sleep(300);
-            playLine1();
-            Thread.sleep(300);
-            playLine2();
-            Thread.sleep(300);
+            Thread.sleep(100);
+            playLine4();
+            Thread.sleep(100);
         }
         catch(Exception e){
             System.out.println(e);
         }
     }
 
+    /*
+     * Megolovania - Undertale - Toby Fox
+     * @author Victoria
+     */
     private void playLine1() {
-        Music.playNote("C");
-        Music.playNote("D");
+        Music.playNote("D5i");
+        Music.playNote("D5i");
+        Music.playNote("D6");
+        Music.playNote("A6");
+        Music.playNote("Ri");
+        Music.playNote("G#6i");
+        Music.playNote("Ri");
+        Music.playNote("G6i");
+        Music.playNote("F6i-");
+        Music.playNote("F6i");
+        Music.playNote("Ri");
+        Music.playNote("D6i");
+        Music.playNote("F6i");
+        Music.playNote("G6i");
     }
 
     private void playLine2() {
-        Music.playNote("C");
-        Music.playNote("D");
+        Music.playNote("C5i");
+        Music.playNote("C5i");
+        Music.playNote("D6");
+        Music.playNote("A6");
+        Music.playNote("Ri");
+        Music.playNote("G#6i");
+        Music.playNote("Ri");
+        Music.playNote("G6i");
+        Music.playNote("F6i-");
+        Music.playNote("F6i");
+        Music.playNote("Ri");
+        Music.playNote("D6i");
+        Music.playNote("F6i");
+        Music.playNote("G6i");
     }
 
     private void playLine3() {
-        Music.playNote("C");
-        Music.playNote("D");
+        Music.playNote("B5i");
+        Music.playNote("B5i");
+        Music.playNote("D6");
+        Music.playNote("A6");
+        Music.playNote("Ri");
+        Music.playNote("G#6i");
+        Music.playNote("Ri");
+        Music.playNote("G6i");
+        Music.playNote("F6i-");
+        Music.playNote("F6i");
+        Music.playNote("Ri");
+        Music.playNote("D6i");
+        Music.playNote("F6i");
+        Music.playNote("G6i");
+    }
+
+    private void playLine4() {
+        Music.playNote("A#5i");
+        Music.playNote("A#5i");
+        Music.playNote("D6");
+        Music.playNote("A6");
+        Music.playNote("Ri");
+        Music.playNote("G#6i");
+        Music.playNote("Ri");
+        Music.playNote("G6i");
+        Music.playNote("F6i-");
+        Music.playNote("F6i");
+        Music.playNote("Ri");
+        Music.playNote("D6i");
+        Music.playNote("F6i");
+        Music.playNote("G6i");
     }
 
     public static void main(String[] args) {
