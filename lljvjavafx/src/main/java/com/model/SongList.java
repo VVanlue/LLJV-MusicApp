e8b6077b28ceed7f43d84b0438278a0f3c5e26e9
VVanlue <victoriavanlue8@gmail.com>
2025-03-03 13:54:47 -0500

    SongList stubs

---
+++ lljvjavafx/src/main/java/com/model/SongList.java
@@ -0,0 +1,46 @@
+package com.model;
+
+public class SongList {
+    private ArrayList<Song> songs;
+    private static SongList instance;
+
+    private SongList() {
+        System.out.println("");
+    }
+
+    public static SongList getInstance() {
+        System.out.println("");
+        return null;
+    }
+
+    public void addSong(Song song) {
+        System.out.println("");
+    }
+
+    public void removeSong(Song song) {
+        System.out.println()
+    }
+
+    public ArrayList<Song> getAllSongs() {
+        System.out.println("");
+        return null;
+    }
+
+    public ArrayList<Song> getSngsByKeyword(String word) {
+        System.out.println("");
+        return null;
+    }
+
+    public void viewSong(Song song) {
+        System.out.println("");
+    }
+
+    public void sortByInstrument() {
+        System.out.println("");
+    }
+
+    public void sortByLevels() {
+        System.out.println("");
+    }
+
+}
