63d5bb27bd962e3d21e903f774e65b2e8f69be1d
laytonturner <143432952+laytonturner@users.noreply.github.com>
2025-03-04 13:52:40 -0500

    Added attributes, methods and construct

---
+++ lljvjavafx/src/main/java/com/model/Tempo.java
@@ -0,0 +1,35 @@
+package com.model;
+import java.util.Scanner;
+
+public class Tempo {
+
+    //attributes
+    private int BPM;
+
+    //constants
+    public Tempo (int BPM)
+    {
+        this.BPM = BPM;
+    }
+
+    //methods
+    public void setBPM(int BPM)
+    {
+        this.BPM = BPM
+    }
+
+    public void increaseBPM (int amount)
+    {
+        this.BPM = BPM + amount;
+    }
+
+    public void decreaseBPM (int amount)
+    {
+        this.BPM = BPM - amount;
+    }
+
+    
+
+
+
+}
