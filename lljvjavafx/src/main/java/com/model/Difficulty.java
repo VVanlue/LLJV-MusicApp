e2b79cf9b71ce0a51dabacf5188b0231700c9681
laytonturner <143432952+laytonturner@users.noreply.github.com>
2025-03-06 13:54:06 -0500

    Just added some variables (it might be wrong tho)

--- lljvjavafx/src/main/java/com/model/Difficulty.java
+++ lljvjavafx/src/main/java/com/model/Difficulty.java
@@ -1,8 +1,12 @@
 package com.model;
-import java.util.Scanner;
 
 public class Difficulty {
 
+    private String difficultiesLevel;
+
+    public static final String EASY = "EASY";
+    public static final String MEDIUM = "MEDIUM";
+    public static final String HARD = "HARD";
 
     
 }
