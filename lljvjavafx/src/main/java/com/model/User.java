3c3c036608543a8074f9630f645c98e2c3da57b6
LGLADNEY2 <LGLADNEY@email.sc.edu>
2025-03-19 17:26:08 -0400

    Resolve conflicts and update classes

--- lljvjavafx/src/main/java/com/model/User.java
+++ lljvjavafx/src/main/java/com/model/User.java
@@ -1,5 +1,22 @@
 package com.model;
 
 public class User {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String emailAddress;
 
+    public void UserLogin(String userName, String password) {
+		this.userName = userName;
+		this.password = password;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+    
+    public String getPassword() {
+        return password;
+    }
 }
