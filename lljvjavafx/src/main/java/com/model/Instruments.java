84e504b558d7d5dee9524b4e49b6a7fc48ff54fd
LGLADNEY2 <LGLADNEY@email.sc.edu>
2025-03-20 11:11:17 -0400

    updated instruments class

--- lljvjavafx/src/main/java/com/model/Instruments.java
+++ lljvjavafx/src/main/java/com/model/Instruments.java
@@ -1,3 +1,6 @@
+/**
+ * Represents a musical instrument used for playing songs.
+ */
 package com.model;
 import java.util.Scanner;
 
@@ -9,27 +12,88 @@ private String stringName;
 private String keyName;
 private boolean isPlaying;
 
+    /**
+     * Constructs an Instrument with the specified details.
+     */
+    public Instruments(String name, String type) {
+        this.name = name;
+        this.type = type;
+        this.isPlaying = false;
+    }
 
-//constructors
-public Instruments(String name, String stringName, String keyName, boolean isPlaying)
-{
-    this.name - name;
-    this.stringName = stringName;
-    this.keyName = keyName;
-    this.isPlaying = false;
-}
+    /**
+     * Gets the instrument's name.
+     */
+    public String getName() {
+        return name;
+    }
 
+    /**
+     * Sets the instrument's name.
+     * @return true if successful, false if the name is null
+     */
+    public boolean setName(String name) {
+        if (name == null) return false;
+        this.name = name;
+        return true;
+    }
 
-//methods
-public String getName()
-{
-    return name;
-}
+    /**
+     * Gets the instrument's type.
+     */
+    public String getType() {
+        return type;
+    }
 
-//confused about this part
-//public void playNote(String note)
-//{
- 
-//}
+    /**
+     * Sets the instrument's type.
+     * @return true if successful, false if the type is null
+     */
+    public boolean setType(String type) {
+        if (type == null) return false;
+        this.type = type;
+        return true;
+    }
 
+    /**
+     * Checks if the instrument is currently playing.
+     */
+    public boolean isPlaying() {
+        return isPlaying;
+    }
+
+    /**
+     * Sets the playing state of the instrument.
+     */
+    public void setPlaying(boolean playing) {
+        isPlaying = playing;
+    }
+
+    /**
+     * Simulates playing the instrument with a specific song and tempo.
+     */
+    public String play(String songName, int tempo) {
+        isPlaying = true;
+        return name + " is playing " + songName + " at " + tempo + " BPM.";
+    }
+
+    /**
+     * Stops playing the instrument.
+     */
+    public String stop() {
+        isPlaying = false;
+        return name + " has stopped playing.";
+    }
+
+    /**
+     * Plays a specific note on the instrument.
+     */
+    public String playNote(String note) {
+        return name + " is playing note: " + note;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + type + ")";
+    }
 }
