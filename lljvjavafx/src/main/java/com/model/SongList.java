33ca6ec06ac3dcf48b21446d77ab9aefae01fa66
VVanlue <victoriavanlue8@gmail.com>
2025-03-24 17:37:13 -0400

    SongList stub #23

--- lljvjavafx/src/main/java/com/model/SongList.java
+++ lljvjavafx/src/main/java/com/model/SongList.java
@@ -1,9 +1,18 @@
 package com.model;
 
+import java.util.ArrayList;
+
 public class SongList {
     private ArrayList<Song> songs;
     private static SongList instance;
 
+    public enum Genres {
+        CLASSICAL, POP, ROCK, JAZZ, SOUL, KIDS
+    }
+
+    public enum Difficulties {
+        EASY, MEDIUM, HARD
+    }
     private SongList() {
         System.out.println("");
     }
