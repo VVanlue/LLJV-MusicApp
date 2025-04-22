64390822c27f007d1d9074b89da2fc4c150e4303
Laurin Johnson <laurincjohnson1@gmail.com>
2025-04-22 12:42:08 -0400

    clean up Dashboard controller

--- src/main/java/com/lljvmusicapp/controller/DashboardController.java
+++ src/main/java/com/lljvmusicapp/controller/DashboardController.java
@@ -2,6 +2,8 @@ package com.lljvmusicapp.controller;
 
 import java.io.IOException;
 
+import com.lljvmusicapp.model.Facade;
+
 import com.lljvmusicapp.model.Lesson;
 import com.lljvmusicapp.model.LessonList;
 import com.lljvmusicapp.model.Song;
@@ -22,65 +24,17 @@ import javafx.stage.Stage;
 public class DashboardController {
 
     @FXML private Label welcomeLabel;
-    @FXML private ListView<String> favSongsList;
-    @FXML private ListView<String> lessonsList;
-    @FXML private ListView<String> songsList;
-    @FXML private Button loginRedirectButton;
+    
+    private Facade facade;
 
-    public void setUser(User user) {
-        if (user == null) {
-            welcomeLabel.setText("Welcome, Guest!");
-            favSongsList.getItems().add("Login to view favorite songs.");
-        } else {
-            welcomeLabel.setText("Welcome, " + user.getFirstName());
-            for (String songId : user.getFavSongs()) {
-                favSongsList.getItems().add("Song ID: " + songId);
-            }
-        }
-
-        for (Lesson lesson : LessonList.getInstance().getLessons()) {
-            lessonsList.getItems().add(lesson.getTitle());
-        }
-
-        for (Song song : SongList.getInstance().getSongs()) {
-            songsList.getItems().add(song.getTitle() + " - " + song.getGenre());
-        }
+    public void setFacade(Facade facade) //BE SURE TO CALL THIS FIRST
+    {
+        this.facade = facade;
     }
 
-    @FXML
-    private void handleReturnToLogin() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/login.fxml"));
-            Parent root = loader.load();
-            Stage stage = (Stage) welcomeLabel.getScene().getWindow();
-            stage.setScene(new Scene(root, 640, 480));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @FXML
-    private void handleStartLessonQuiz() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/lesson.fxml"));
-            Parent root = loader.load();
-            Stage stage = (Stage) welcomeLabel.getScene().getWindow();
-            stage.setScene(new Scene(root, 640, 480));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @FXML
-    private void handleGoToSongScreen(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/song.fxml"));
-            Scene songScene = new Scene(loader.load());
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(songScene);
-            stage.setTitle("Play Notes");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void setWelcomeLabel()
+    {
+        String username = this.facade.getCurrentUser().getUsername();
+        welcomeLabel.setText("Welcome, " + username + "!");
     }
 }
