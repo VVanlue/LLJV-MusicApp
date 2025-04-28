7ca0c9e70ce00f2d4864348e26e26ee8fed326b5
VVanlue <victoriavanlue8@gmail.com>
2025-04-27 23:47:34 -0400

    Coloring

--- src/main/java/com/lljvmusicapp/App.java
+++ src/main/java/com/lljvmusicapp/App.java
@@ -1,5 +1,6 @@
 package com.lljvmusicapp;
 
+import java.io.File;
 import java.io.IOException;
 
 import javafx.application.Application;
@@ -17,14 +18,23 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("login"), 640, 480);
+        scene = new Scene(loadFXML("login"), 900, 600);
+        
+        File cssFile = new File("src/main/resources/styles.css");
+        scene.getStylesheets().add(cssFile.toURI().toString());
+        
+        stage.setTitle("Victoria's Music App");
         stage.setScene(scene);
         stage.show();
     }
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
-    }
+
+        File cssFile = new File("src/main/resources/styles.css");
+        scene.getStylesheets().clear(); 
+        scene.getStylesheets().add(cssFile.toURI().toString());
+     }
 
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/" + fxml + ".fxml"));
